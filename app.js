dbabbb5fa4dee6af6dcfaad5d1a4a057c95841f1
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-25 09:06:08 +0300

    Update theme saving function

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ const three = document.getElementById('three');
 const body = document.body;
 
 // Apply cached theme on reload
+function applyTheme(theme) {
+    if (theme === 'one') {
+      body.classList.remove('two', 'three');
 
-const theme = localStorage.getItem('theme');
-const radioButtons = document.querySelectorAll('input[name="option"]');
-
-
-if (theme) {
-    body.classList.add(theme);
-    for (const radio of radioButtons) {
-        if (radio.value === theme){
-            radio.checked = true;
-            break;
-        }
+    } else if (theme === 'two') {
+      body.classList.remove('one', 'three');
+    } else if (theme === 'three') {
+      body.classList.remove('one', 'two');
     }
-}
+    body.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  
+    // Check the radio button
+    const radio = document.getElementById(theme);
+    if (radio) {
+      radio.checked = true;
+    }
+  }
+  
+  // Check the previously selected theme on page load
+  document.addEventListener("DOMContentLoaded", () => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'one' || theme === 'two' || theme === 'three') {
+      applyTheme(theme);
+    }
+  });
 
 // Event listeners
 
+one.addEventListener('click', () => applyTheme('one'));
+two.addEventListener('click', () => applyTheme('two'));
+three.addEventListener('click', () => applyTheme('three'));
+
+/* 
 one.addEventListener('click', ()=>{
     if (body.classList.contains('two')){
         body.classList.replace('two', 'one');
@@ -53,4 +70,4 @@ three.addEventListener('click', ()=>{
         one.checked = false;
     }
     localStorage.setItem('theme', 'three');
-})
+}) */
